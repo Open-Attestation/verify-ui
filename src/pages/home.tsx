fb82b3176b020535742741c36a5aa8720dc973c6
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-13 18:10:32 +0800

    feat: setup home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const HomePage: React.FunctionComponent = () => {
-  return <h1>home page</h1>;
+  return (
+    <div className="bg-gray-100">
+      <h1>home page</h1>
+    </div>
+  );
 };

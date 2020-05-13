2c88efd47ca85ae20b1396567aadb90a63a828c5
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-13 16:53:45 +0800

    feat: setup base content

--- integration/integration.spec.ts
+++ integration/integration.spec.ts
@@ -1,19 +1,7 @@
-import { Selector } from "testcafe";
+import "testcafe";
 
-fixture("Counter Game").page`http://localhost:3000`;
+fixture("Verify documents").page`http://localhost:3000`;
 
-const Counter = Selector(".counter div").nth(0);
-const Increment = Selector(".counter button").nth(0);
-const Decrement = Selector(".counter button").nth(1);
-
-test("Custom certificate is rendered correctly", async (t) => {
-  await t.expect(Counter.textContent).eql("Counter: 0");
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.click(Increment);
-  await t.expect(Counter.textContent).eql("Counter: 4");
-  await t.click(Decrement);
-  await t.click(Decrement);
-  await t.expect(Counter.textContent).eql("Counter: 2");
+test("test", async (t) => {
+  await t.expect(true).eql(true);
 });

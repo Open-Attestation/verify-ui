5b0bbbe0dcc603dd2f6eb677721a22dd434c79fd
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-15 14:16:40 +0800

    feat: setup draggable document

--- src/components/DraggableDemoCertificate.tsx
+++ src/components/DraggableDemoCertificate.tsx
@@ -4,7 +4,7 @@ import ROPSTEN_DEMO from "./Ropsten-Demo.json";
 export const DraggableDemoCertificate: React.FunctionComponent = () => (
   <div className="w-32 h-32 m-32">
     <div draggable onDragStart={() => console.log("dragged")}>
-      <a href={`data:text/plain;,${JSON.stringify(ROPSTEN_DEMO, null, 2)}`}">
+      <a href={`data:text/plain;,${JSON.stringify(ROPSTEN_DEMO, null, 2)}`}>
         <img
           data-testid="demo-cert"
           style={{ cursor: "grabbing" }}

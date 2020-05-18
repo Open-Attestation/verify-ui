68784e698b378e5f8c81626a67b524b6d4e86c69
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-18 16:15:58 +0800

    feat: simple dropzone

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Dropzone from "react-dropzone";
-import styles from "./dropzone.module.css";
 
 export const DropZone: React.FunctionComponent = () => {
   return (
     <Dropzone onDrop={(acceptedFile) => alert(`The file ${acceptedFile[0].name} will be processed`)}>
       {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps({ className: styles.dropzoneStyle })}>
+        <div
+          {...getRootProps()}
+          className="h-84 w-84 p-16 text-center border-dashed border-gray-500 border-2 bg-gray-200 flex flex-col justify-center"
+        >
           <input {...getInputProps()} />
-          <p>Drag and drop your tradetrust file</p>
+          <p className="text-gray-700">Drag and drop your tradetrust file</p>
         </div>
       )}
     </Dropzone>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import { DropZone } from "../components/DropZone";
 export const HomePage: React.FunctionComponent = () => {
   return (
     <div className="bg-gray-100">
-      <h1>home page</h1>
+      <h1 className="mb-20 text-xl">Home page</h1>
       <DropZone />
     </div>
   );

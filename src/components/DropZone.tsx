383b351c16b63256f912d29706cb9a889538b3b5
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-05-20 20:06:23 +0800

    feat: add file checking (#6)

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 
+const onFileDrop = (files: File[]): void => {
+  const reader = new FileReader();
+
+  reader.onerror = () => {
+    alert(`The file uploaded is not a valid Open Attesation file, error: ${reader.error}`);
+  };
+
+  reader.onload = () => {
+    try {
+      if (reader.result && typeof reader.result === "string") {
+        JSON.parse(reader.result);
+        alert(`The file ${files[0].name} will be processed`);
+      } else {
+        alert(`The file uploaded is not a valid Open Attesation file`);
+      }
+    } catch (e) {
+      alert(`The file uploaded is not a valid Open Attesation file, error: ${e.message}`);
+    }
+  };
+
+  reader.readAsBinaryString(files[0]);
+};
+
 export const DropZone: React.FunctionComponent = () => {
   return (
-    <Dropzone onDrop={(acceptedFile) => alert(`The file ${acceptedFile[0].name} will be processed`)}>
+    <Dropzone onDrop={onFileDrop}>
       {({ getRootProps, getInputProps }) => (
         <div
           {...getRootProps()}

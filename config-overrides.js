4aa71df30104b2fa1a952ad2b5443ae6c9df180e
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-26 16:44:21 +0800

    feat: add document renderer

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,6 @@
-const { override, addDecoratorsLegacy } = require("customize-cra");
-
-const enableRequireEnsure = () => (config) => {
+module.exports = function override(config, env) {
   config.output.globalObject = "this";
   config.module.rules[0].parser.requireEnsure = true;
+
   return config;
 };
-
-module.exports = override(addDecoratorsLegacy(), enableRequireEnsure());

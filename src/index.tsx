aa4735e3701d0fda9b346eb4361a8623f75e480f
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-26 20:16:31 +0800

    feat: add document renderer

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 // it will fail in the ci because the file may not exist => disabled
 // eslint-disable-next-line import/no-unresolved
 import "./index.css";
-import { App } from "./App";
+import { App } from "./app";
 
 ReactDOM.render(
   <React.StrictMode>

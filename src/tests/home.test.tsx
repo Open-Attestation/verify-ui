6b138cc09eaf11e190898a61950d41eaab2545ce
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-05-27 14:14:35 +0800

    feat: max width dropzone (#10)

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -6,7 +6,7 @@ describe("home page", () => {
   it("renders learn react link", () => {
     expect.assertions(1);
     const { getByText } = render(<HomePage />);
-    const text = getByText("Drag and drop your tradetrust file");
+    const text = getByText("Drag and Drop your verifiable credentials");
     expect(text).toBeInTheDocument();
   });
 });

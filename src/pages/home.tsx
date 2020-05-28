6eeef7f8df757e1ace6b7f16c5ff7845b21089a7
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-28 11:52:32 +0800

    feat: display document status

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,12 +7,12 @@ export const HomePage: React.FunctionComponent = () => {
   const [rawDocument, setRawDocument] = useState<WrappedDocument>();
   const [documentStatus, setDocumentStatus] = useState<boolean>();
 
-  const statusBanner = (status: "valid" | "invalid"): React.ReactElement => {
-    const bannerColor = status === "valid" ? "bg-green-200" : "bg-red-300";
-    const textColor = status === "valid" ? "text-green-600" : "text-red-600";
+  const statusBanner = (verified: boolean): React.ReactElement => {
+    const bannerColor = verified ? "bg-green-200" : "bg-red-300";
+    const textColor = verified ? "text-green-600" : "text-red-600";
     return (
       <div className={`w-full text-center py-3 ${bannerColor}`}>
-        <p className={`${textColor} font-medium`}>Verifiable Credential is {status}</p>
+        <p className={`${textColor} font-medium`}>Verifiable Credential is {verified ? "valid" : "invalid"}</p>
       </div>
     );
   };
@@ -20,7 +20,7 @@ export const HomePage: React.FunctionComponent = () => {
   return (
     <div className="bg-gray-100">
       <DropZone setRawDocument={(doc) => setRawDocument(doc)} setDocumentStatus={(bool) => setDocumentStatus(bool)} />
-      {documentStatus !== undefined && (documentStatus ? statusBanner("valid") : statusBanner("invalid"))}
+      {documentStatus !== undefined && statusBanner(documentStatus)}
       {rawDocument ? <DocumentRenderer rawDocument={rawDocument} /> : null}
     </div>
   );

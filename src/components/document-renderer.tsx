3f73211dbbc761c35b302c7cca886053c86d8511
Benjamin Lim <36101433+benjaminlim00@users.noreply.github.com>
2020-05-29 17:00:17 +0800

    feat: add multi tabs (#14)

--- src/components/document-renderer.tsx
+++ src/components/document-renderer.tsx
@@ -1,6 +1,7 @@
 import { FrameActions, FrameConnector, HostActionsHandler } from "@govtechsg/decentralized-renderer-react-components";
 import { getData, WrappedDocument } from "@govtechsg/open-attestation";
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useMemo } from "react";
+import { Tabs } from "./tabs";
 
 interface DocumentRendererProps {
   rawDocument: WrappedDocument;
@@ -9,25 +10,39 @@ interface DocumentRendererProps {
 export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = ({
   rawDocument,
 }: DocumentRendererProps) => {
-  const document = getData(rawDocument);
+  const document = useMemo(() => getData(rawDocument), [rawDocument]);
+
+  const [templates, setTemplates] = useState<{ id: string; label: string }[]>([]);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+
+  const updateTemplates = useCallback((templates) => {
+    setTemplates(templates);
+    setSelectedTemplate(templates[0].id);
+  }, []);
 
   const [toFrame, setToFrame] = useState<HostActionsHandler | null>();
-  const [height, setHeight] = useState();
+  const [height, setHeight] = useState(0);
   const onConnected = useCallback((toFrame: HostActionsHandler) => {
     // wrap into a function otherwise toFrame function will be executed
     setToFrame(() => toFrame);
   }, []);
 
-  const fromFrame = useCallback((action: FrameActions): void => {
-    if (action.type === "UPDATE_HEIGHT") {
-      setHeight(action.payload);
-    }
-  }, []);
+  const fromFrame = useCallback(
+    (action: FrameActions): void => {
+      if (action.type === "UPDATE_HEIGHT") {
+        setHeight(action.payload);
+      }
+      if (action.type === "UPDATE_TEMPLATES") {
+        updateTemplates(action.payload);
+      }
+    },
+    [updateTemplates]
+  );
 
   // let's set the frame action to null every time a new document is passed down, the actions will be set once the connection established
   useEffect(() => {
     setToFrame(null);
-  }, [document]);
+  }, [rawDocument]);
 
   useEffect(() => {
     if (toFrame) {
@@ -40,13 +55,32 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
     }
   }, [document, toFrame]);
 
+  useEffect(() => {
+    if (toFrame && selectedTemplate) {
+      // TODO: how can i get this to run? selected template is updated on new tab click but toFrame is null so it does not run.
+      toFrame({
+        type: "SELECT_TEMPLATE",
+        payload: selectedTemplate,
+      });
+    }
+  }, [selectedTemplate, toFrame]);
+
   return (
-    <FrameConnector
-      style={{ height: `${height}px`, width: "100%", border: "0px" }}
-      source={document.$template.url}
-      onConnected={onConnected}
-      dispatch={fromFrame}
-      key={document.id} // we will reset the connection every time the id change (react will mount a new component)
-    />
+    <>
+      {templates.length > 0 && (
+        <Tabs
+          templates={templates}
+          selectedTemplate={selectedTemplate}
+          onSelectTemplate={(selectedTemplate) => setSelectedTemplate(selectedTemplate)}
+        />
+      )}
+      <FrameConnector
+        style={{ height: `${height}px`, width: "100%", border: "0px" }}
+        source={document.$template.url}
+        onConnected={onConnected}
+        dispatch={fromFrame}
+        key={document.id} // we will reset the connection every time the id change (react will mount a new component)
+      />
+    </>
   );
 };

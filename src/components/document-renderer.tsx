a72c143b8e420e02d13561c386b090a6dea2bda8
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-29 13:45:41 +0800

    feat: add multi tabs

--- src/components/document-renderer.tsx
+++ src/components/document-renderer.tsx
@@ -1,6 +1,7 @@
 import { FrameActions, FrameConnector, HostActionsHandler } from "@govtechsg/decentralized-renderer-react-components";
 import { getData, WrappedDocument } from "@govtechsg/open-attestation";
 import React, { useState, useCallback, useEffect } from "react";
+import { MultiTabs } from "./multi-tabs";
 
 interface DocumentRendererProps {
   rawDocument: WrappedDocument;
@@ -10,19 +11,32 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
   rawDocument,
 }: DocumentRendererProps) => {
   const document = getData(rawDocument);
+  const [templates, setTemplates] = useState([]);
+  const [selectedTemplate, setSelectedTemplate] = useState("");
+
+  const updateTemplates = useCallback((templates) => {
+    setTemplates(templates);
+    setSelectedTemplate(templates[0].id);
+  }, []);
 
   const [toFrame, setToFrame] = useState<HostActionsHandler | null>();
-  const [height, setHeight] = useState();
+  const [height, setHeight] = useState<number>();
   const onConnected = useCallback((toFrame: HostActionsHandler) => {
     // wrap into a function otherwise toFrame function will be executed
     setToFrame(() => toFrame);
   }, []);
 
-  const fromFrame = useCallback((action: FrameActions): void => {
-    if (action.type === "UPDATE_HEIGHT") {
-      setHeight(action.payload);
-    }
-  }, []);
+  const fromFrame = useCallback(
+    (action: FrameActions): void => {
+      if (action.type === "UPDATE_HEIGHT") {
+        setHeight(action.payload);
+      }
+      if (action.type === "UPDATE_TEMPLATES") {
+        updateTemplates(action.payload);
+      }
+    },
+    [updateTemplates]
+  );
 
   // let's set the frame action to null every time a new document is passed down, the actions will be set once the connection established
   useEffect(() => {
@@ -40,13 +54,30 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
     }
   }, [document, toFrame]);
 
+  useEffect(() => {
+    if (toFrame && selectedTemplate) {
+      // TODO: how can i get this to run?
+      toFrame({
+        type: "SELECT_TEMPLATE",
+        payload: selectedTemplate,
+      });
+    }
+  }, [selectedTemplate, toFrame]);
+
   return (
-    <FrameConnector
-      style={{ height: `${height}px`, width: "100%", border: "0px" }}
-      source={document.$template.url}
-      onConnected={onConnected}
-      dispatch={fromFrame}
-      key={document.id} // we will reset the connection every time the id change (react will mount a new component)
-    />
+    <>
+      <MultiTabs
+        templates={templates}
+        selectedTemplate={selectedTemplate}
+        onSelectTemplate={(selectedTemplate) => setSelectedTemplate(selectedTemplate)}
+      />
+      <FrameConnector
+        style={{ height: `${height}px`, width: "100%", border: "0px" }}
+        source={document.$template.url}
+        onConnected={onConnected}
+        dispatch={fromFrame}
+        key={document.id} // we will reset the connection every time the id change (react will mount a new component)
+      />
+    </>
   );
 };
---
+++ src/components/multi-tabs.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface MultiTabsProps {
+  templates: { id: string; label: string }[];
+  selectedTemplate: string;
+  onSelectTemplate: (id: string) => void;
+}
+
+export const MultiTabs: React.FunctionComponent<MultiTabsProps> = ({
+  selectedTemplate,
+  templates,
+  onSelectTemplate,
+}) => {
+  return (
+    <ul className="flex flex row bg-gray-100 justify-center py-8">
+      {templates && templates.length > 0
+        ? templates.map(({ id, label }) => (
+            <li key={id} className="cursor-pointer font-medium">
+              <button
+                className={id === selectedTemplate ? "border-b border-black border-solid px-6 py-2 " : "px-6 py-2 "}
+                id={id}
+                onClick={() => {
+                  onSelectTemplate(id);
+                }}
+                role="tab"
+                aria-controls="home"
+                aria-selected="true"
+              >
+                {label}
+              </a>
+            </li>
+          ))
+        : null}
+    </ul>
+  );
+};

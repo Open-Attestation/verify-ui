1a103926239d1951d04bb8698595f02caf9c416c
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-05-29 16:40:31 +0800

    feat: add multi tabs

--- src/components/document-renderer.tsx
+++ src/components/document-renderer.tsx
@@ -1,7 +1,7 @@
 import { FrameActions, FrameConnector, HostActionsHandler } from "@govtechsg/decentralized-renderer-react-components";
 import { getData, WrappedDocument } from "@govtechsg/open-attestation";
 import React, { useState, useCallback, useEffect, useMemo } from "react";
-import { MultiTabs } from "./multi-tabs";
+import { Tabs } from "./tabs";
 
 interface DocumentRendererProps {
   rawDocument: WrappedDocument;
@@ -12,7 +12,7 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
 }: DocumentRendererProps) => {
   const document = useMemo(() => getData(rawDocument), [rawDocument]);
 
-  const [templates, setTemplates] = useState([]);
+  const [templates, setTemplates] = useState<{ id: string; label: string }[]>([]);
   const [selectedTemplate, setSelectedTemplate] = useState("");
 
   const updateTemplates = useCallback((templates) => {
@@ -21,7 +21,7 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
   }, []);
 
   const [toFrame, setToFrame] = useState<HostActionsHandler | null>();
-  const [height, setHeight] = useState<number>();
+  const [height, setHeight] = useState(0);
   const onConnected = useCallback((toFrame: HostActionsHandler) => {
     // wrap into a function otherwise toFrame function will be executed
     setToFrame(() => toFrame);
@@ -67,11 +67,13 @@ export const DocumentRenderer: React.FunctionComponent<DocumentRendererProps> = 
 
   return (
     <>
-      <MultiTabs
-        templates={templates}
-        selectedTemplate={selectedTemplate}
-        onSelectTemplate={(selectedTemplate) => setSelectedTemplate(selectedTemplate)}
-      />
+      {templates.length > 0 && (
+        <Tabs
+          templates={templates}
+          selectedTemplate={selectedTemplate}
+          onSelectTemplate={(selectedTemplate) => setSelectedTemplate(selectedTemplate)}
+        />
+      )}
       <FrameConnector
         style={{ height: `${height}px`, width: "100%", border: "0px" }}
         source={document.$template.url}

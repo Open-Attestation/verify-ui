bbaa915f34c9e8a766f5ffcfc0635e1297e80803
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-06-03 12:51:43 +0800

    feat: setup demo doc

---
+++ src/components/demo-doc.tsx
@@ -0,0 +1,19 @@
+import { WrappedDocument } from "@govtechsg/open-attestation";
+import React from "react";
+import ROPSTEN_DEMO from "./wrapped/wrapped-demo.json";
+
+interface DemoDocProps {
+  setRawDocument: (doc: WrappedDocument) => void;
+}
+
+export const DemoDoc: React.FunctionComponent<DemoDocProps> = ({ setRawDocument }: DemoDocProps) => {
+  const onClick = (): void => {
+    const json = JSON.parse(JSON.stringify(ROPSTEN_DEMO));
+    setRawDocument(json);
+  };
+  return (
+    <button className="w-64 p-16 border-gray-500 border-solid border-4 text-gray-700" onClick={onClick}>
+      Click here to test with a demo certificate
+    </button>
+  );
+};

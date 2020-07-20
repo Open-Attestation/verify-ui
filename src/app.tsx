18353b501e009498f59fe4ed9aeb63c23786687a
Nebulis <nebounet@gmail.com>
2020-07-20 16:23:38 +0800

    chore: add react-router

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/home";
 
-export const App: React.FunctionComponent = () => <HomePage />;
+export const App: React.FunctionComponent = () => (
+  <Router>
+    <Switch>
+      <Route path="/verify">
+        <HomePage />
+      </Route>
+      <Route path="/">
+        <div>Hello world</div>
+      </Route>
+    </Switch>
+  </Router>
+);

a5d27ac96cd7d738d0522704c915fc969b4fb7f3
Raymond Yeh <ray@geek.sg>
2020-11-24 11:18:22 +0800

    chore: int test for dnsdid

---
+++ integration/verified-issued-dnsdid.spec.ts
@@ -0,0 +1,9 @@
+import { uploadDocument, validateIssuer, validateIframeText } from "./helper";
+
+fixture("Verified issued").page`http://localhost:3000`;
+
+test("Document should be verified correctly", async () => {
+  await uploadDocument("./certificate-issued-dnsdid.json");
+  await validateIssuer("donotverify.testing.verify.gov.sg");
+  await validateIframeText("RT-PCR SWAB TEST RESULT");
+});

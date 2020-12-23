37a8791c209170fe1e2a8e64ee3ee595737b9e37
Sim Boon Long <simboonlong@gmail.com>
2020-12-23 17:12:23 +0800

    feat: footer gds logo (#55)

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,16 +1,38 @@
 import styled from "@emotion/styled";
 import React from "react";
 import { Link } from "react-router-dom";
+import gdsLogo from "./images/logo-colored-dark-on-light.svg";
 import govtechLogo from "./images/logo_govtech_hort.gif";
 
 const GovtechImage = styled.img`
-  width: 197px;
+  width: 200px;
 `;
 
 export const Footer: React.FunctionComponent = () => (
-  <footer className="flex flex-col items-center m-5">
+  <footer className="flex flex-col items-center text-center m-5">
     <h6>verify.gov.sg is brought to you by Government Technology Agency:</h6>
-    <GovtechImage className="mb-4" src={govtechLogo} alt="govtech logo" />
+    <div className="flex flex-wrap items-center my-4">
+      <div className="w-full sm:w-auto">
+        <a
+          className="inline-block transition-opacity hover:opacity-75"
+          href="https://www.tech.gov.sg/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GovtechImage src={govtechLogo} alt="govtech logo" />
+        </a>
+      </div>
+      <div className="w-full sm:w-auto">
+        <a
+          className="inline-block transition-opacity hover:opacity-75"
+          href="https://hive.tech.gov.sg/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GovtechImage src={gdsLogo} alt="Government Digital Services logo" />
+        </a>
+      </div>
+    </div>
     <p className="small">©2020 Government of Singapore | Last updated 23rd May 2020</p>
     <p className="small">v1.0.0-{process.env.REACT_APP_COMMIT_REF?.substring(0, 6)}</p>
     <p className="small">

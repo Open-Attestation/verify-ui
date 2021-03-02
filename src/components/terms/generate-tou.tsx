afe9ab7ea6c64f8130bb1348a0692065d4091afa
eugbyte <eugenetham1994@gmail.com>
2021-03-02 16:29:36 +0800

    chore: deleted old code, fix lint

--- src/components/terms/generate-tou.tsx
+++ src/components/terms/generate-tou.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 
 function recurselyGenerateList(clauses: string[]): JSX.Element {
+  const results: JSX.Element[] = [];
 
-    const results: JSX.Element[] = [];
-
-    for (const clause of clauses) {
-        if (Array.isArray(clause)) {
-            const result: JSX.Element = recurselyGenerateList(clause);
-            results.push(result);
-            continue;
-        }
-        results.push(<li>{clause}</li>)
+  for (const clause of clauses) {
+    if (Array.isArray(clause)) {
+      const result: JSX.Element = recurselyGenerateList(clause);
+      results.push(result);
+      continue;
     }
-    return <ol className="ol-nested">
-        {results}
-    </ol>
+    results.push(<li>{clause}</li>);
+  }
+  return <ol className="ol-nested">{results}</ol>;
 }
 
 export function generateTOU(doc: Record<string, string[]>): JSX.Element {
-
-    return <div>
-        {Object.entries(doc).map(([heading, clauses]) => <div>
-            <h4 className="text-primary mb-2">{heading}</h4>
-            {recurselyGenerateList(clauses)}
-        </div>)}
+  return (
+    <div>
+      {Object.entries(doc).map(([heading, clauses], index) => (
+        <div key={index}>
+          <h4 className="text-primary mb-2">{heading}</h4>
+          {recurselyGenerateList(clauses)}
+        </div>
+      ))}
     </div>
-    
+  );
 }

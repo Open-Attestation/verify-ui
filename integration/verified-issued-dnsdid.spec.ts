2f2dad6d434d73e86f42841fc339d3354c7b8e0d
Nebulis <nebounet@gmail.com>
2021-06-09 15:47:55 +0800

    chore: monitor infura

--- integration/verified-issued-dnsdid.spec.ts
+++ integration/verified-issued-dnsdid.spec.ts
@@ -5,5 +5,5 @@ fixture("Verified issued").page`http://localhost:3000`;
 test("Document should be verified correctly", async () => {
   await uploadDocument("./certificate-issued-dnsdid.json");
   await validateIssuer("donotverify.testing.verify.gov.sg");
-  await validateIframeText("RT-PCR SWAB TEST RESULT");
+  await validateIframeText("Reverse transcription polymerase chain reaction (rRT-PCR) test");
 });

be6aeee27f4117ede5e249504351966468831fc7
Nebulis <nebounet@gmail.com>
2021-11-25 11:49:34 +0000

    fix: :)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  target: "serverless", // for https://github.com/netlify/netlify-plugin-nextjs/tree/v3#readme
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.

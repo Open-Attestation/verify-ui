05dbe7758812df99c2de4374f609f99f0990358b
lucas_yap <yapyuyou@hotmail.com>
2021-11-25 16:53:26 +0800

    fix: add netlify plugin config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  distDir: "out",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
+  target: "serverless",
 };

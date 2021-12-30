52945c948685ba3b6d438aa889d7c548eb620930
eugbyte <eugenetham1994@gmail.com>
2021-12-30 11:08:16 +0800

    chore: added error handling and customEvenHook

--- src/services/google-analytics.ts
+++ src/services/google-analytics.ts
@@ -1,14 +1,34 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
+import { UaEventOptions } from "react-ga4/types/ga4";
+import useDeepCompareEffect from "use-deep-compare-effect";
 
 const GTAG_ID = process.env.REACT_APP_GTAG_ID;
 
 export const useGoogleAnalytics = (): void => {
   /* Initialise Google Analytics 4 if GTAG_ID is provided */
   useEffect(() => {
-    if (GTAG_ID?.startsWith("G-")) {
-      ReactGA.initialize(GTAG_ID);
-      ReactGA.send("pageview");
+    try {
+      if (GTAG_ID?.startsWith("G-")) {
+        ReactGA.initialize(GTAG_ID);
+        ReactGA.send("pageview");
+      }
+    } catch (error) {
+      console.log(error);
     }
   }, []);
+};
+
+interface GAEvent extends UaEventOptions {
+  [arg: string]: any;
+}
+// custom hook to send google analytics custom event
+export const useGaEvent = (params: GAEvent): void => {
+  useDeepCompareEffect(() => {
+    try {
+      ReactGA.event(params);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [params]);
 };

b2da5a2e3faaf5c829a0ff84838cead93c6f6749
eugbyte <eugenetham1994@gmail.com>
2021-12-31 10:17:09 +0800

    chore: update

--- src/services/google-analytics.ts
+++ src/services/google-analytics.ts
@@ -1,7 +1,8 @@
+import { VerificationFragment } from "@govtechsg/oa-verify";
+import { OpenAttestationDocument } from "@govtechsg/open-attestation";
+import get from "lodash.get";
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
-import { UaEventOptions } from "react-ga4/types/ga4";
-import useDeepCompareEffect from "use-deep-compare-effect";
 
 const GTAG_ID = process.env.REACT_APP_GTAG_ID;
 
@@ -9,26 +10,70 @@ export const useGoogleAnalytics = (): void => {
   /* Initialise Google Analytics 4 if GTAG_ID is provided */
   useEffect(() => {
     try {
+      console.log(GTAG_ID);
       if (GTAG_ID?.startsWith("G-")) {
         ReactGA.initialize(GTAG_ID);
         ReactGA.send("pageview");
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }, []);
 };
 
-interface GAEvent extends UaEventOptions {
-  [arg: string]: any;
+enum HEALTHCERT_TYPE {
+  PDT = "PDT",
+  VAC = "VAC",
 }
-/* custom hook to send google analytics custom event whenever custom event changes */
-export const useGaEvent = (params: GAEvent): void => {
-  useDeepCompareEffect(() => {
-    try {
-      ReactGA.event(params);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [params]);
+
+const isVac = (data: OpenAttestationDocument): boolean => get(data, "name") === "VaccinationHealthCert";
+const isPDT = (data: OpenAttestationDocument): boolean =>
+  get(data, "name") === "HealthCert" || get(data, "version") === "pdt-healthcert-v2.0";
+export const isHealthCert = (data: OpenAttestationDocument): boolean => isVac(data) || isPDT(data);
+
+export const getHealthCertType = (data: OpenAttestationDocument): HEALTHCERT_TYPE | "" => {
+  if (isVac(data)) {
+    return HEALTHCERT_TYPE.VAC;
+  } else if (isPDT(data)) {
+    return HEALTHCERT_TYPE.PDT;
+  }
+  return "";
+};
+
+enum EVENT_CATEGORY {
+  VERIFIED = "certificate_verified",
+  ERROR = "certificate_error",
+}
+
+export const sendHealthCertVerifiedEvent = (data: OpenAttestationDocument): void => {
+  if (!isHealthCert(data)) {
+    return;
+  }
+  try {
+    ReactGA.event(EVENT_CATEGORY.VERIFIED, {
+      document_id: data.id ?? "",
+      document_type: getHealthCertType(data),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const sendHealthCertErrorEvent = (data: OpenAttestationDocument, fragments: VerificationFragment[]): void => {
+  if (!isHealthCert(data)) {
+    return;
+  }
+  const message: string = fragments
+    .filter((fragment) => fragment.status === "ERROR")
+    .map((fragment) => `INVALID ${fragment.type}: ${(fragment as any)?.reason?.message}`)
+    .join(". ");
+  try {
+    ReactGA.event(EVENT_CATEGORY.ERROR, {
+      document_id: data.id ?? "",
+      document_type: getHealthCertType(data),
+      errors: message,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };

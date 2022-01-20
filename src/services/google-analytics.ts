a39bb7cb80e83996bfb005bed340abff37e475aa
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-01-20 16:49:01 +0800

    chore: add comments

--- src/services/google-analytics.ts
+++ src/services/google-analytics.ts
@@ -3,12 +3,17 @@ import { OpenAttestationDocument } from "@govtechsg/open-attestation";
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
 
+/**
+ * Initialise Google Analytics 4 only once (if GTAG_ID is provided)
+ *
+ * The ReactGA.send("pageview") is explicitly called afterwards because
+ * the pageview event is not automatically triggered on first initialisation
+ * but will be automatically triggered by the GA script on subsequent page views.
+ */
 export const useGoogleAnalytics = (): void => {
-  /* Initialise Google Analytics 4 if GTAG_ID is provided */
   useEffect(() => {
     try {
       const GTAG_ID = process.env.REACT_APP_GTAG_ID;
-      console.log(GTAG_ID);
       if (GTAG_ID?.startsWith("G-")) {
         ReactGA.initialize(GTAG_ID);
         ReactGA.send("pageview");

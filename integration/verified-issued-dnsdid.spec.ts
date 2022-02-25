293af0a1372dd1645c3dd8b07d8e0ab9fd7df1e6
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-02-25 12:07:53 +0800

    chore: npx create-next-app@latest --typescript

--- integration/verified-issued-dnsdid.spec.ts
+++
@@ -1,9 +0,0 @@
-import { uploadDocument, validateIssuer, validateIframeText } from "./helper";
-
-fixture("Verified issued").page`http://localhost:3000`;
-
-test("Document should be verified correctly", async () => {
-  await uploadDocument("./certificate-issued-dnsdid.json");
-  await validateIssuer("donotverify.testing.verify.gov.sg");
-  await validateIframeText("Reverse transcription polymerase chain reaction (rRT-PCR) test");
-});
--- integration/verified-issued.spec.ts
+++
@@ -1,9 +0,0 @@
-import { uploadDocument, validateIssuer, validateIframeText } from "./helper";
-
-fixture("Verified issued").page`http://localhost:3000`;
-
-test("Document should be verified correctly", async () => {
-  await uploadDocument("./certificate-issued-ropsten.json");
-  await validateIssuer("example.openattestation.com");
-  await validateIframeText("John Doe");
-});

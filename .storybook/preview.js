6a59407ca568d056d07bcb299c92ac30be11ac54
David <david_tjokroaminoto@tech.gov.sg>
2022-03-01 13:38:24 +0800

    fix: audit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import "../src/index.css";
 import "./storybook.css";
-import { addDecorator } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+// import { addDecorator } from "@storybook/react";
+// import { withKnobs } from "@storybook/addon-knobs";
 
-addDecorator(withKnobs);
+// addDecorator(withKnobs);

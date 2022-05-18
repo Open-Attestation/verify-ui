3644cbf4e94505fc5c3ac6163c42687b44641140
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-05-18 10:58:58 +0800

    fix: apply custom headers via next.config.js (#115)
    * fix: apply custom headers via next.config.js
    
    * fix: custom headers for static pages must still be supplied by Netlify with _headers file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-let SITE_URL = "https://www.verify.gov.sg"; // Default
+const { generateHeaders } = require("./next.headers");
+
+let SITE_URL;
 
 // Env variables by Netlify (https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)
 if (process.env.CONTEXT !== "production" && process.env.DEPLOY_PRIME_URL) {
   SITE_URL = process.env.DEPLOY_PRIME_URL;
 } else if (process.env.NODE_ENV !== "production") {
   SITE_URL = `http://localhost:3000`;
+} else {
+  SITE_URL = "https://www.verify.gov.sg";
 }
 
 // Env variables by Netlify (https://docs.netlify.com/configure-builds/environment-variables/#git-metadata)
@@ -14,10 +18,12 @@ const COMMIT_REF = process.env.COMMIT_REF || "v1.0.0";
 const nextConfig = {
   reactStrictMode: true,
   env: {
+    CONTEXT: process.env.CONTEXT,
     SITE_URL,
     COMMIT_REF,
     BUILD_DATE: new Date().toISOString(),
   },
+  headers: generateHeaders,
 };
 
 module.exports = nextConfig;

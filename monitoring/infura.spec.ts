e34e79b64b680ced821cc28a710cced16d8a5df8
David <david_tjokroaminoto@tech.gov.sg>
2022-05-24 10:43:47 +0800

    added check infura

--- monitoring/infura.spec.ts
+++ monitoring/infura.spec.ts
@@ -14,7 +14,7 @@ export const getBody = async (body: Buffer): Promise<string> =>
   });
 
 const statsLogger = RequestLogger(
-  { url: "https://infura.io/api/stats/request-volume", method: "post" },
+  { url: "https://infura.io/api/stats/request-activity", method: "post" },
   {
     logResponseBody: true,
   }
@@ -26,18 +26,29 @@ const sleep = (timeout: number) => new Promise((resolve) => setTimeout(resolve, 
 const getRequestCount = async (requestLogger: RequestLogger) => {
   let tries = 30;
   while (tries-- > 0) {
+    let requestCountTotal = 0;
     for (const request of requestLogger.requests) {
       const body = request.response?.body;
       if (!body) continue;
       const parsedBody = JSON.parse(Buffer.isBuffer(body) ? await getBody(body) : body);
       const time = parsedBody?.data?.[0]?.query?.time;
-      // there are multiple stats call. We only want the one for the past 24h
       if (time?.from === "24h" && time?.tick === "h") {
         const results = parsedBody.data[0].result;
-        return results.reduce((acc: any, cur: any) => acc + cur.value, 0);
+
+        for (let i = 0; i < results.length; i++) {
+          let reqcount = results[i].value;
+          requestCountTotal += reqcount;
+        }
+
+
+        if (requestCountTotal > 0){
+          console.log("total api calls : " + requestCountTotal);
+          return requestCountTotal;
+        }
       }
     }
-    await sleep(1000);
+    
+    await sleep(5000);
   }
   throw new Error("Could not find the stats");
 };
@@ -53,23 +64,39 @@ const openGithubIssue = async (requestCount: number) => {
   });
 };
 
+const notifySlack = async (requestCount: number, threshold: number) => {
+  await fetch(process.env.SLACK_WEBHOOK, {
+    method: "POST",
+    body: JSON.stringify({ 
+      text: `ALERT - ${requestCount} REQUESTS ON INFURA, current API limit is ${threshold}` 
+    }),
+  });
+};
+
 test("Status check should reflect error correctly", async (t) => {
   // login
   await t.typeText(Selector("[data-testid='field_email']"), process.env.INFURA_EMAIL ?? "");
   await t.typeText(Selector("[data-testid='field_password']"), process.env.INFURA_PASSWORD ?? "");
   await t.click(Selector("[data-testid='auth-button']"));
 
-  // wait to see the create project button
-  await Selector("[data-testid='create-project']").with({ visibilityCheck: true })();
+  // wait to be logged in
+  await Selector("[data-testid='header-user-icon']");
 
-  // go to verify.gov.sg project
-  await t.click(Selector("p").withText("verify.gov.sg")).addRequestHooks(statsLogger);
+  await t.navigateTo("https://infura.io/dashboard/stats/ethereum/24h/all-projects").addRequestHooks(statsLogger);
+
+  // wait to see the create project button
+  await Selector("[data-testid='create-project']");
 
   const requestCount = await getRequestCount(statsLogger);
-  const maximumRequest = 1000000;
+  const maximumRequest = Number(String(process.env.INFURA_THRESHOLD)) ?? 1000000;
   const thresholdPercentage = 60; // 60%
   const threshold = (maximumRequest * thresholdPercentage) / 100;
   if (requestCount > threshold) {
-    await openGithubIssue(requestCount);
+    if (process.env.SLACK_WEBHOOK){
+      await notifySlack(requestCount, threshold);
+    }
+    else {
+      await openGithubIssue(requestCount);
+    }
   }
 });

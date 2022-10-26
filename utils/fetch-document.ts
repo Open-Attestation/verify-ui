af30e9ae5ec84bb4e49efdb6e8abec416444b9c4
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-10-26 15:05:19 +0800

    feat: introduce util to decode uri fragment

--- utils/fetch-document.ts
+++ utils/fetch-document.ts
@@ -11,12 +11,29 @@ const decodeAndParseUri = (encoded: string) => {
 };
 
 /**
+ * Helper function to validate and parse OA-embedded uri fragment
+ */
+export const decodeUriFragment = (encodedHash: string) => {
+  let decoded: v2.WrappedDocument | v3.WrappedDocument;
+
+  try {
+    decoded = decodeAndParseUri(encodedHash);
+  } catch (e) {
+    console.error(e);
+    throw new CodedError("QueryParamsError", "The URI fragment is malformed so the document cannot be loaded");
+  }
+
+  if (!validateSchema(decoded)) {
+    throw new CodedError("InvalidDocumentError", "The URI fragment contains an invalid OpenAttestation document");
+  }
+
+  return decoded;
+};
+
+/**
  * Helper function to validate and parse query params + hash
  *
  * See more: https://github.com/Open-Attestation/adr/blob/master/universal_actions.md
- * @param encodedQuery ActionUrlQueryRecord
- * @param encodedHash ActionUrlAnchorRecord
- * @returns [ParsedQuery, ParsedAnchor]
  */
 export const decodeQueryAndHash = (encodedQuery: string, encodedHash?: string) => {
   let decodedQ: Static<typeof ActionUrlQueryRecord>;

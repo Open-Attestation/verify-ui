f53130a3f4db3100c027e1301e2071217d949883
Lucas <31716292+yapyuyou@users.noreply.github.com>
2022-12-15 16:42:14 +0800

    fix: typo

--- pages/qr.tsx
+++ pages/qr.tsx
@@ -29,11 +29,12 @@ const Qr: NextPage = () => {
           .filter((device) => device.kind === "videoinput")
           .map((device, n) => {
             console.log(`Device ${n}: ` + JSON.stringify(device, null, 2));
-            if (device.label.toLocaleLowerCase().includes("face")) {
+            if (device.label.toLocaleLowerCase().includes("back")) {
               setMediaModesFound(mediaModesFound.map((mediaMode, i) => (i === 0 ? device.deviceId : mediaMode)));
               setLog(`FOUND ${device.deviceId}`);
             } else if (device.label.toLocaleLowerCase().includes("front")) {
               setMediaModesFound(mediaModesFound.map((mediaMode, i) => (i === 1 ? device.deviceId : mediaMode)));
+              setLog(`FOUND ${device.deviceId}`);
             }
           });
         setIsLoaded(true);

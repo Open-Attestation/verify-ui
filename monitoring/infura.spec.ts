308518a0f6ba1860c7f8c94caf0702438f6d61cc
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-12-19 14:28:44 +0800

    fix: attach logger to fixture

--- monitoring/infura.spec.ts
+++ monitoring/infura.spec.ts
@@ -19,7 +19,8 @@ const statsLogger = RequestLogger(
     logResponseBody: true,
   }
 );
-fixture("Infura Monitoring").page`https://infura.io/login`;
+
+fixture("Infura Monitoring").page`https://infura.io/login`.requestHooks(statsLogger);
 
 const sleep = (timeout: number) => new Promise((resolve) => setTimeout(resolve, timeout));
 
@@ -67,7 +68,7 @@ test("Status check should reflect error correctly", async (t) => {
   await Selector("[data-testid='create-project']").with({ visibilityCheck: true })();
 
   // go to verify.gov.sg project
-  await t.click(Selector("p").withText("verify.gov.sg")).addRequestHooks(statsLogger);
+  await t.click(Selector("p").withText("verify.gov.sg"));
 
   const requestCount = await getRequestCount(statsLogger);
   const maximumRequest = 1000000;

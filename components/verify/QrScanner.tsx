909ca42052d1e1456eca653faabf55b6b8222836
Lucas <31716292+yapyuyou@users.noreply.github.com>
2023-01-09 17:08:51 +0800

    test: mobile 5

--- components/verify/QrScanner.tsx
+++ components/verify/QrScanner.tsx
@@ -26,7 +26,7 @@ export const QrScanner: React.FC<QrScannerProps> = ({ currentMode, deviceIds, re
     <>
       <QrReader
         constraints={{ facingMode: isFrontCamera ? "user" : "environment" }}
-        videoContainerStyle={{ paddingTop: isMobile ? "140%" : "60%" }}
+        videoContainerStyle={{ paddingTop: isMobile ? "140%" : "10%", border: "1px solid red" }}
         videoStyle={{ width: "unset", borderRadius: "0.5rem", margin: "auto", left: 0, right: 0 }}
         onResult={handleOnResult}
       />

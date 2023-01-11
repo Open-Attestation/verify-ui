50720b8904721242e46bdac90e05e321aaa12d3c
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-01-11 16:25:42 +0800

    fix: change query param name to m=uri-fragment

--- utils/get-universal-action-type.test.ts
+++ utils/get-universal-action-type.test.ts
@@ -4,8 +4,8 @@ describe("getUniversalActionType", () => {
   it("should return NONE", async () => {
     expect(getUniversalActionType()).toBe("NONE");
     expect(getUniversalActionType({})).toBe("NONE");
-    expect(getUniversalActionType({ q: undefined, uriFragment: undefined })).toBe("NONE");
-    expect(getUniversalActionType({ uriFragment: "" })).toBe("NONE");
+    expect(getUniversalActionType({ q: undefined, m: undefined })).toBe("NONE");
+    expect(getUniversalActionType({ m: "" })).toBe("NONE");
   });
 
   it("should return HOSTED_URL", async () => {
@@ -15,8 +15,8 @@ describe("getUniversalActionType", () => {
   });
 
   it("should return EMBEDDED_URI_FRAGMENT", async () => {
-    expect(getUniversalActionType({ q: "", uriFragment: "true" })).toBe("EMBEDDED_URI_FRAGMENT");
-    expect(getUniversalActionType({ q: "{}", uriFragment: "true" })).toBe("EMBEDDED_URI_FRAGMENT");
-    expect(getUniversalActionType({ q: "somestring", uriFragment: "true" })).toBe("EMBEDDED_URI_FRAGMENT");
+    expect(getUniversalActionType({ q: "", m: "uri-fragment" })).toBe("EMBEDDED_URI_FRAGMENT");
+    expect(getUniversalActionType({ q: "{}", m: "uri-fragment" })).toBe("EMBEDDED_URI_FRAGMENT");
+    expect(getUniversalActionType({ q: "somestring", m: "uri-fragment" })).toBe("EMBEDDED_URI_FRAGMENT");
   });
 });
--- utils/get-universal-action-type.ts
+++ utils/get-universal-action-type.ts
@@ -2,12 +2,12 @@ type UniversalActionType = "NONE" | "HOSTED_URL" | "EMBEDDED_URI_FRAGMENT";
 
 interface UniversalActionQuery {
   q?: string;
-  uriFragment?: string;
+  m?: string;
 }
 
 export const getUniversalActionType = (query?: UniversalActionQuery): UniversalActionType => {
   const hasHostedUrl = typeof query?.q === "string";
-  const hasUriFragment = query?.uriFragment === "true";
+  const hasUriFragment = query?.m === "uri-fragment";
 
   if (hasUriFragment) return "EMBEDDED_URI_FRAGMENT";
   else if (hasHostedUrl) return "HOSTED_URL";

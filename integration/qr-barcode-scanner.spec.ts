b2e7fd1e92c7c0a80c248529556f3c05dafd2d47
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-02-07 16:39:34 +0800

    test: add integration tests for qr barcode scanner

---
+++ integration/qr-barcode-scanner.spec.ts
@@ -0,0 +1,70 @@
+import { Selector } from "testcafe";
+import { validateIframeText, validateIssuer } from "./helper";
+
+fixture("Load Scan QR Page").page`http://localhost:3000/qr`.beforeEach(async (t) => {
+  const SwitchToBarcodeScannerLink = Selector("span").withText("Switch to Barcode Scanner");
+  await t.click(SwitchToBarcodeScannerLink);
+});
+
+const StatusCheck = Selector("[data-testid='verification-checks']");
+
+test("Barcode scanner on QR page should be on standby for input", async (t) => {
+  await t.expect(Selector("p").withText("Ready, waiting for scan").exists).ok();
+
+  await t.dispatchEvent(Selector("body"), "blur");
+  await t.expect(Selector("p").withText("Please click here to begin").exists).ok();
+
+  await t.dispatchEvent(Selector("body"), "focus");
+  await t.expect(Selector("p").withText("Ready, waiting for scan").exists).ok();
+});
+
+test("Barcode scanner on QR page should be able to detect keyboard input", async (t) => {
+  await t.expect(Selector("img[alt='Scan Icon']").exists).ok();
+  await t.dispatchEvent(Selector("body"), "keydown", { key: "t" });
+  await t.expect(Selector("svg[data-icon='circle-notch']").exists).ok();
+  await t.wait(300).expect(Selector("img[alt='Scan Icon']").exists).ok();
+});
+
+test("Barcode scanner on QR page should be able to verify a valid QR", async (t) => {
+  const action = {
+    type: "DOCUMENT",
+    payload: {
+      uri: "https://schemata.openattestation.com/sg/gov/moh/pdt-healthcert/2.0/endorsed-wrapped.json",
+      permittedActions: ["VIEW"],
+      redirect: "https://www.verify.gov.sg/verify",
+    },
+  };
+
+  const validVerifyUrl = `https://www.verify.gov.sg/verify?q=${encodeURI(JSON.stringify(action))}`;
+
+  for (let char of validVerifyUrl) {
+    await t.dispatchEvent(Selector("body"), "keydown", { key: char });
+  }
+
+  await t.pressKey("enter");
+
+  await t.switchToWindow((w: WindowFilterData) => w.url.host === "www.verify.gov.sg");
+
+  await validateIssuer("donotverify.testing.verify.gov.sg");
+  await t.expect(StatusCheck.withText("Document has not been tampered").exists).ok();
+  await t.expect(StatusCheck.withText("Document has been issued").exists).ok();
+  await t.expect(StatusCheck.withText("Issued by Singapore Government").exists).ok();
+  await validateIframeText("Tan Chen Chen");
+});
+
+test("Barcode scanner on QR page should throw error when verifying an invalid QR", async (t) => {
+  await t.setNativeDialogHandler(() => true);
+
+  const invalidVerifyUrl = `https://www.fake-verify.com/verify?q=fake`;
+
+  for (let char of invalidVerifyUrl) {
+    await t.dispatchEvent(Selector("body"), "keydown", { key: char });
+  }
+
+  await t.pressKey("enter");
+
+  const [dialogHistory] = await t.getNativeDialogHistory();
+
+  await t.expect(dialogHistory.type).eql("alert");
+  await t.expect(dialogHistory.text).eql("Invalid Verify QR, please try again");
+});

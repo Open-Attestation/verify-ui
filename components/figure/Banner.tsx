40f20384ba104a785499e2d8deb45c8963f260a3
MehulDShah <mehul.dash@gmail.com>
2023-04-24 10:30:49 +0800

    feat: changed font weight

--- components/figure/Banner.tsx
+++ components/figure/Banner.tsx
@@ -4,7 +4,7 @@ export const InformationBanner: React.FC = () => (
   <section className="py-4 bg-blue-50">
     <div className="container items-center gap-10">
       <p className="inline text-indigo-800 text-sm md:text-base space-x-1.5">
-        <span className="font-light">
+        <span className="font-normal">
           You can now scan the QR code on your .oa document via camera or barcode scanner 🎉
         </span>
         <Link href="/qr">

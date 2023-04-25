445d7f2a0684c546c3f61fb14e4a9383ab74d406
MehulDShah <mehul.dash@gmail.com>
2023-04-25 15:31:25 +0800

    feat: amendedment to banner action

--- components/figure/Banner.tsx
+++ components/figure/Banner.tsx
@@ -13,7 +13,7 @@ export const InformationBanner: React.FC = () => (
             target="_blank"
             rel="noreferrer noopener"
           >
-            Learn more.
+            Try it now.
           </a>
         </Link>
       </p>

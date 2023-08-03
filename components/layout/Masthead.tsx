18c6e73d6d13d05f5a5d0de3b4fb3b03e67eb89c
Tng Zi Xiang <54768054+zixiang2018@users.noreply.github.com>
2023-08-03 09:44:09 +0800

    feat: 185540545 update masthead (#177)
    * feat: update masthead
    
    * chore: attempt upgrading browser tool for build

--- components/layout/Masthead.tsx
+++ components/layout/Masthead.tsx
@@ -1,23 +1,12 @@
+import "@govtechsg/sgds-masthead/dist/sgds-masthead/sgds-masthead.css";
+import { SgdsMasthead } from "@govtechsg/sgds-masthead-react";
+
 const Masthead: React.FC = () => {
   return (
-    <header className="bg-gray-100">
+    <header className="bg-[#EFEFEF]">
       <div className="container">
         <div className="flex flex-wrap py-0.5">
-          <a
-            className="no-underline text-black hover:text-black cursor-pointer"
-            href="https://www.gov.sg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/crest.svg"
-              alt="Singapore lion head symbol"
-              width="14"
-              height="14"
-              className="inline-block align-middle"
-            />
-            <span className="text-sm pl-2">A Singapore Government Agency Website</span>
-          </a>
+          <SgdsMasthead />
         </div>
       </div>
     </header>
@@ -25,16 +14,3 @@ const Masthead: React.FC = () => {
 };
 
 export default Masthead;
-
-/* <header id="sgds-masthead" className="sgds-masthead">
-    <div className="container">
-      <div className="flex flex-wrap">
-        <div className="w-full">
-          <a href="https://www.gov.sg" target="_blank" rel="noopener noreferrer">
-            <span className="sgds-icon sgds-icon-sg-crest" />
-            <span className="is-text">A Singapore Government Agency Website</span>
-          </a>
-        </div>
-      </div>
-    </div>
-  </header> */

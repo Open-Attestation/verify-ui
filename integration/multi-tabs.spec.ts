a6751dc9fd72617ee74a6e36685814d2d9b8516b
Tng Zi Xiang <tngzixiang@gmail.com>
2023-08-16 16:09:29 +0800

    chore: swap order of tests

--- integration/multi-tabs.spec.ts
+++ integration/multi-tabs.spec.ts
@@ -3,14 +3,14 @@ import { uploadDocument, validateIssuer, validateIframeText } from "./helper";
 
 fixture("Multi tabs").page`http://localhost:3000`;
 
-const tabMain = Selector("[data-testid='renderer-tab']", { timeout: 100000 }).withText("Main");
+const tabMain = Selector("[data-testid='renderer-tab']", { timeout: 40000 }).withText("Main");
 const tabPdf = Selector("[data-testid='renderer-tab']").withText("dummy.pdf");
 
 test("Multi tabs should be rendered correctly", async () => {
   await uploadDocument("./certificate-attachments-goerli.json");
   await validateIssuer("example.openattestation.com");
-  await t.expect(tabMain.exists).ok();
   await validateIframeText("John Doe");
   await t.expect(tabPdf.exists).ok();
+  await t.expect(tabMain.exists).ok();
   await t.click(tabPdf);
 });

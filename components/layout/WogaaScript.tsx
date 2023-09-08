bac13ceec50dbe0f242838070a48354f3e774bc8
Tng Zi Xiang <tngzixiang@gmail.com>
2023-09-08 18:09:30 +0800

    chore: add wogaa script into page

---
+++ components/layout/WogaaScript.tsx
@@ -0,0 +1,19 @@
+import { useGoogleAnalytics } from "@utils/google-analytics";
+import Script from "next/script";
+
+const WOGAA_ENV = process.env.NEXT_PUBLIC_WOGAA_ENV === "production";
+
+function WogaaScript() {
+  useGoogleAnalytics();
+  return (
+    <>
+      {WOGAA_ENV ? (
+        <Script src="https://assets.wogaa.sg/scripts/wogaa.js" async />
+      ) : (
+        <Script src="https://assets.dcube.cloud/scripts/wogaa.js" async />
+      )}
+    </>
+  );
+}
+
+export default WogaaScript;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,29 +7,13 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 
 import { useGoogleAnalytics } from "@utils/google-analytics";
 import defaultSeoConfig from "../next-seo.config";
-import { useRouter } from "next/router";
-import Script from "next/script";
 
 config.autoAddCss = false;
 
-const WOGAA_ENV = process.env.NEXT_PUBLIC_WOGAA_ENV === "production";
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const { asPath } = router;
-
   useGoogleAnalytics();
   return (
     <>
-      {/* /verify has to scrub URI fragments before loading wogaa */}
-
-      {asPath !== "/verify" &&
-        (WOGAA_ENV ? (
-          <Script src="https://assets.wogaa.sg/scripts/wogaa.js" async />
-        ) : (
-          <Script src="https://assets.dcube.cloud/scripts/wogaa.js" async />
-        ))}
       <DefaultSeo {...defaultSeoConfig} />
       <Component {...pageProps} />
     </>

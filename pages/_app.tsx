3a7649f3ab3be54bd441a150bc0ff37335526ade
Tng Zi Xiang <tngzixiang@gmail.com>
2023-09-08 16:31:06 +0800

    chore: use asPath without useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { useGoogleAnalytics } from "@utils/google-analytics";
 import defaultSeoConfig from "../next-seo.config";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Script from "next/script";
 
 config.autoAddCss = false;
@@ -17,19 +16,15 @@ const WOGAA_ENV = process.env.NEXT_PUBLIC_WOGAA_ENV === "production";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const [path, setPath] = useState<string>("");
 
-  useEffect(() => {
-    const { asPath } = router;
-    setPath(asPath);
-  }, [router]);
+  const { asPath } = router;
 
   useGoogleAnalytics();
   return (
     <>
       {/* /verify has to scrub URI fragments before loading wogaa */}
 
-      {path !== "/verify" &&
+      {asPath !== "/verify" &&
         (WOGAA_ENV ? (
           <Script src="https://assets.wogaa.sg/scripts/wogaa.js" async />
         ) : (

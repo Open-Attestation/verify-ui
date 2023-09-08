f56a49725aaded765472f6070911943a610e54d2
Tng Zi Xiang <tngzixiang@gmail.com>
2023-09-08 15:41:18 +0800

    chore: do not auto load wogaa on verify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,34 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 
 import { useGoogleAnalytics } from "@utils/google-analytics";
 import defaultSeoConfig from "../next-seo.config";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Script from "next/script";
 
 config.autoAddCss = false;
 
+const WOGAA_ENV = process.env.NEXT_PUBLIC_WOGAA_ENV === "production";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [path, setPath] = useState<string>("");
+
+  useEffect(() => {
+    const { asPath } = router;
+    setPath(asPath);
+  }, [router]);
+
   useGoogleAnalytics();
   return (
     <>
+      {/* /verify has to scrub URI fragments before loading wogaa */}
+
+      {path !== "/verify" &&
+        (WOGAA_ENV ? (
+          <Script src="https://assets.wogaa.sg/scripts/wogaa.js" async />
+        ) : (
+          <Script src="https://assets.dcube.cloud/scripts/wogaa.js" async />
+        ))}
       <DefaultSeo {...defaultSeoConfig} />
       <Component {...pageProps} />
     </>

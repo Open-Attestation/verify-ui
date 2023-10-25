94235b7f4b91e350d5085a9b26a20819c54f3dcb
Tng Zi Xiang <tngzixiang@gmail.com>
2023-10-25 16:05:29 +0800

    chore: update tests

--- integration/wogaa-script.spec.ts
+++ integration/wogaa-script.spec.ts
@@ -59,13 +59,13 @@ test("Wogaa should not capture key data in HOSTED_URL", async (t) => {
     )
     .addRequestHooks(logger)
     .wait(2000);
-
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
-  await t.expect(capturedUrlString).eql(`http://localhost:3000/verify`);
+  await t
+    .expect(
+      logger.contains((record) =>
+        record.request.url.includes(`/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2Fverify`)
+      )
+    )
+    .eql(true);
 });
 
 test("Wogaa should not capture information in URI_FRAGMENT", async (t) => {
@@ -73,66 +73,59 @@ test("Wogaa should not capture information in URI_FRAGMENT", async (t) => {
     .navigateTo(`http://localhost:3000/verify?m=uri-fragment#${encodeURI(JSON.stringify(sampleCert))}`)
     .addRequestHooks(logger)
     .wait(2000);
-
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
-  await t.expect(capturedUrlString).eql(`http://localhost:3000/verify`);
+  await t
+    .expect(
+      logger.contains((record) =>
+        record.request.url.includes(`/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2Fverify`)
+      )
+    )
+    .eql(true);
 });
 
 test("Wogaa should capture correct URL when there is no universal actions", async (t) => {
   await t.navigateTo(`http://localhost:3000/verify`).addRequestHooks(logger).wait(2000);
-
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
-  await t.expect(capturedUrlString).eql(`http://localhost:3000/verify`);
+  await t
+    .expect(
+      logger.contains((record) =>
+        record.request.url.includes(`/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2Fverify`)
+      )
+    )
+    .eql(true);
 });
 
-test("Wogaa should capture correct URL for non-verify pages", async (t) => {
+test("Wogaa should capture entire URL for non-verify pages", async (t) => {
+  await t.navigateTo(`http://localhost:3000/faq`).addRequestHooks(logger).wait(2000);
+  await t
+    .expect(
+      logger.contains((record) =>
+        record.request.url.includes(`/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2Ffaq`)
+      )
+    )
+    .eql(true);
+});
+
+test("Wogaa should capture entire URL for non-verify pages", async (t) => {
   await t
     .navigateTo(`http://localhost:3000/faq?m=uri-fragment#${encodeURI(JSON.stringify(sampleCert))}`)
     .addRequestHooks(logger)
     .wait(2000);
 
-  // TODO: remove this
-  logger.requests.forEach((req) => console.log(req.request.url));
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
   await t
-    .expect(capturedUrlString)
-    .eql(
-      `http://localhost:3000/faq?m=uri-fragment#%7B%22version%22:%22https://schema.openattestation.com/2.0/schema.json%22,%22data%22:%7B%22$template%22:%7B%22name%22:%22b2eb315a-a074-467c-b544-bc1a1abec466:string:main%22,%22type%22:%22734e6906-6808-4463-a395-d7e49d425138:string:EMBEDDED_RENDERER%22,%22url%22:%224395f24d-7fd1-432a-acce-66e19c224688:string:https://tutorial-renderer.openattestation.com%22%7D,%22recipient%22:%7B%22name%22:%22e87d55bd-3566-4abd-8335-3bfa481d0a7e:string:John%20Doe%22%7D,%22issuers%22:%5B%7B%22name%22:%22c3e5dce6-32ce-470e-893a-9deb3980fd93:string:Demo%20Issuer%22,%22documentStore%22:%22b94de29f-bf2c-4e8a-938f-6fe8b7993d1e:string:0xdAA795D7664F341B21125206e2e416AEB99e1841%22,%22identityProof%22:%7B%22type%22:%22e17172cc-de99-441c-82aa-422b74f6b4f8:string:DNS-TXT%22,%22location%22:%221b0499cf-976c-4750-8632-6cacc2a3bae8:string:example.openattestation.com%22%7D%7D%5D%7D,%22signature%22:%7B%22type%22:%22SHA3MerkleProof%22,%22targetHash%22:%22919534bc6cdf6f05f29f87fc59423959c027a2f8926bb743eef884d53ed4eaf4%22,%22proof%22:%5B%5D,%22merkleRoot%22:%22919534bc6cdf6f05f29f87fc59423959c027a2f8926bb743eef884d53ed4eaf4%22%7D%7D`
-    );
+    .expect(
+      logger.contains((record) =>
+        record.request.url.includes(
+          `/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2Ffaq%3Fm%3Duri-fragment%23%257B%2522version%2522%3A%2522https%3A%2F%2Fschema.openattestation.com%2F2.0%2Fschema.json%2522%2C%2522data%2522%3A%257B%2522%24template%2522%3A%257B%2522name%2522%3A%2522b2eb315a-a074-467c-b544-bc1a1abec466%3Astring%3Amain%2522%2C%2522type%2522%3A%2522734e6906-6808-4463-a395-d7e49d425138%3Astring%3AEMBEDDED_RENDERER%2522%2C%2522url%2522%3A%25224395f24d-7fd1-432a-acce-66e19c224688%3Astring%3Ahttps%3A%2F%2Ftutorial-renderer.openattestation.com%2522%257D%2C%2522recipient%2522%3A%257B%2522name%2522%3A%2522e87d55bd-3566-4abd-8335-3bfa481d0a7e%3Astring%3AJohn%2520Doe%2522%257D%2C%2522issuers%2522%3A%255B%257B%2522name%2522%3A%2522c3e5dce6-32ce-470e-893a-9deb3980fd93%3Astring%3ADemo%2520Issuer%2522%2C%2522documentStore%2522%3A%2522b94de29f-bf2c-4e8a-938f-6fe8b7993d1e%3Astring%3A0xdAA795D7664F341B21125206e2e416AEB99e1841%2522%2C%2522identityProof%2522%3A%257B%2522type%2522%3A%2522e17172cc-de99-441c-82aa-422b74f6b4f8%3Astring%3ADNS-TXT%2522%2C%2522location%2522%3A%25221b0499cf-976c-4750-8632-6cacc2a3bae8%3Astring%3Aexample.openattestation.com%2522%257D%257D%255D%257D%2C%2522signature%2522%3A%257B%2522type%2522%3A%2522SHA3MerkleProof%2522%2C%2522targetHash%2522%3A%2522919534bc6cdf6f05f29f87fc59423959c027a2f8926bb743eef884d53ed4eaf4%2522%2C%2522proof%2522%3A%255B%255D%2C%2522merkleRoot%2522%3A%2522919534bc6cdf6f05f29f87fc59423959c027a2f8926bb743eef884d53ed4eaf4%2522%257D%257D`
+        )
+      )
+    )
+    .eql(true);
 });
 
-test("Wogaa should load on non-verify pages", async (t) => {
-  await t.navigateTo(`http://localhost:3000/faq`).addRequestHooks(logger).wait(2000);
-
-  // TODO: remove this
-  logger.requests.forEach((req) => console.log(req.request.url));
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
-  await t.expect(capturedUrlString).eql(`http://localhost:3000/faq`);
-});
-
-test("Wogaa should load on homepage", async (t) => {
+test("Wogaa should capture entire URL for homepage", async (t) => {
   await t.addRequestHooks(logger).wait(2000);
-
-  // ensure wogaa (1) loads, (2) and a url is being captured
-  await t.expect(logger.requests.length).eql(2);
-  const wogaaUrl = logger.requests.filter((req) => req.request.url.includes("?url="))[0].request.url;
-  // ensure wogaa only capture scrubbed url
-  const capturedUrlString = new URL(wogaaUrl).searchParams.get("url");
-  await t.expect(capturedUrlString).eql(`http://localhost:3000/`);
+  await t
+    .expect(
+      logger.contains((record) => record.request.url.includes(`/scripts/wogaa.js?url=http%3A%2F%2Flocalhost%3A3000%2F`))
+    )
+    .eql(true);
 });

e5f7852ed1e69f15831757c6658a3f25c9cef084
Tng Zi Xiang <54768054+zixiang2018@users.noreply.github.com>
2023-11-22 11:32:34 +0800

    chore: use site_url env var (#191)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 const { generateHeaders } = require("./next.headers");
 
-let SITE_URL;
-
 const withTM = require("next-transpile-modules")(["@govtechsg/sgds-masthead-react"]);
-
-// Env variables by Netlify (https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)
-if (process.env.CONTEXT !== "production" && process.env.DEPLOY_PRIME_URL) {
-  SITE_URL = process.env.DEPLOY_PRIME_URL;
-} else if (process.env.NODE_ENV !== "production") {
-  SITE_URL = `http://localhost:3000`;
-} else {
-  SITE_URL = "https://www.verify.gov.sg";
-}
 
 // Env variables by Netlify (https://docs.netlify.com/configure-builds/environment-variables/#git-metadata)
 const COMMIT_REF = process.env.COMMIT_REF || "v1.0.0";
@@ -23,7 +12,7 @@ const nextConfig = withTM({
   output: "standalone",
   env: {
     CONTEXT: process.env.CONTEXT,
-    SITE_URL,
+    SITE_URL: process.env.SITE_URL,
     COMMIT_REF,
     BUILD_DATE: new Date().toISOString(),
   },

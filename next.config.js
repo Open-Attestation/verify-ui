b312c85d96c00d22b4b514e52cb785d077110e80
tngzixiang <tng_zi_xiang@tech.gov.sg>
2024-05-06 10:02:02 +0800

    chore: set env var in next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
     // Computed env variables that are exposed to browser goes here:
     // https://nextjs.org/docs/app/api-reference/next-config-js/env
     BUILD_DATE: new Date().toISOString(),
+    SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     // Other env variables can be set in .env or .env.production (https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#default-environment-variables)
   },
   headers: generateHeaders,
